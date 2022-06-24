b2586c3200ffc72fc9e399610f1898a02e89e5dd
daniNKV <nikiforovdaniel@gmail.com>
2022-06-24 03:03:22 -0300

    Test del juego

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,38 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('Test gameplay', () => {
+  before(() => {
+    cy.visit('http://192.168.0.90:8080')
   })
+
+  describe('Inicio', () => {
+    it('Existe botón para iniciar el juego', () => {
+      cy.get('#iniciar-juego')
+    })
+    it('Inicia el juego correctamente', () => {
+      cy.get('#iniciar-juego').click()
+      cy.get('#tablero > .juego__boton').should('have.length', 4)
+      cy.get('.juego__info').should('contain','Juega Simon')
+    })
+  })
+  describe('Juego', () => {
+    beforeEach(() => {
+      cy.get('.activo').as('botonCorrecto')
+
+    })
+    
+    it('El click correcto pasa de ronda', () => {
+      cy.get('.juego__info').as('mensajes').should('contain','Juega usted')
+      cy.get('@botonCorrecto').click()
+      cy.get('@mensajes').should('contain','Juega Simon')
+    })
+    it('Clickea incorrecto', () => {
+      cy.get('.juego__info').as('mensajes').should('contain','Juega usted')
+
+      cy.get('@botonCorrecto').siblings('.juego__boton').first().click()
+
+      cy.get('@mensajes').should('contain', 'Perdiste en la ronda 2')
+    })
+
+  })
+
 })
